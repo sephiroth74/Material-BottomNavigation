86c9c7d6d8a4845fab4155909c1adbbd7d5d757b
Alessandro Crugnola <crugnola@adobe.com>
2016-04-11 11:48:13 -0400

    elevation for tablets

--- bottom-navigation/src/main/java/it/sephiroth/android/library/bottomnavigation/BottomNavigation.java
+++ bottom-navigation/src/main/java/it/sephiroth/android/library/bottomnavigation/BottomNavigation.java
@@ -1,15 +1,15 @@
 /**
  * The MIT License (MIT)
- * <p>
+ * <p/>
  * Copyright (c) 2016 Alessandro Crugnola
- * <p>
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
  * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
  * is furnished to do so, subject to the following conditions:
- * <p>
+ * <p/>
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * <p>
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
  * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
@@ -402,7 +402,7 @@ public class BottomNavigation extends FrameLayout implements OnItemClickListener
         final LayerDrawable layerDrawable;
 
         final boolean tablet = isTablet(gravity);
-        final int elevation = !tablet ? getResources().getDimensionPixelSize(R.dimen.bbn_elevation) : 0;
+        final int elevation = getResources().getDimensionPixelSize(!tablet ? R.dimen.bbn_elevation : R.dimen.bbn_elevation_tablet);
         final int bgResId = !tablet ? R.drawable.bbn_background
             : (MiscUtils.isGravityRight(gravity) ? R.drawable.bbn_background_tablet_right : R.drawable.bbn_background_tablet_left);
         final int paddingBottom = !tablet ? shadowHeight : 0;
